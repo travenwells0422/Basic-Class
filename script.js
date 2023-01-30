e7da2bffe24f61eb90cfa9346e18eb128fcac0da
travenwells0422 <travendwellsSoftwareDeveloper@outlook.com>
2023-01-30 04:05:21 -0500

    pushing up to inlineStyling

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ const employee = new Employee("Traven", "29", "Software Developer");
 console.log(employees.details());
 console.warn(employee.description());
 
+// DOM Manipulation
 const title = document.createElement("h1");
 document.body.appendChild(title);
 title.innerHTML = "Parent and Child Class";
@@ -39,3 +40,18 @@ employeesClass.innerText = employee.details();
 const employeeClass = document.createElement("h2");
 document.body.appendChild(employeeClass);
 employeeClass.innerText = employee.description();
+
+const header = document.createElement("header");
+document.body.appendChild(header);
+
+//inline - Styling
+const body = document.querySelector("body");
+body.style.backgroundColor = "gray";
+
+title.style.background = "orange";
+title.style.height = "60px";
+title.style.paddingTop = "25px";
+title.style.textAlign = "center";
+title.style.borderStyle = "solid";
+title.style.borderRadius = "5rem";
+title.style.borderWidth = "10px";
